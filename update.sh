3b1dc97836af3ec0b4d9175ef151b20aeaf30c13
一姬 <kazuki@kazami.cn>
2024-05-31 14:43:25 +0800

    增加更新功能

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@ curl -Ls https://raw.githubusercontent.com/Kazuki-yiji/Gitbackup/main/Gitbackup.
 #使用临时文件中的数据替换原脚本中的内容
 sed -i "/echo 'Please note: GitHub is part of Microsoft';/,/Install #第一次运行/{/echo 'Please note: GitHub is part of Microsoft';/b;/Install #第一次运行/!d;};/Install #第一次运行/r new_data.txt" Gitbackup.sh
 if [ $? -eq 0 ]; then
-    echo "脚本更新成功!"
+    echo "Gitbackup.sh脚本更新成功!"
 	# 删除多余的行
 	sed -i '1,50{/\[ "\$IFInstall" != "ok" \] && Install #第一次运行/d}' Gitbackup.sh
 	sed -i '0,/echo '\''Please note: GitHub is part of Microsoft'\'';/ { //d; }' Gitbackup.sh
