a1f58b8c94f5f66d19f63d03facbf73744156b34
一姬 <kazuki@kazami.cn>
2024-05-31 14:52:58 +0800

    增加更新功能

--- GitbakUpdate.sh
+++ GitbakUpdate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-curl -Ls https://raw.githubusercontent.com/Kazuki-yiji/Gitbackup/main/Gitbackup.sh | sed -n "/echo 'Please note: GitHub is part of Microsoft';/,/Install #第一次运行/p" > new_data.txt
+curl -Ls https://raw.githubusercontent.com/Kazuki-yiji/Gitbackup/main/Gitbackup.sh | sed -n "/echo 'Please note: GitHub is part of Microsoft';/,/Install #第一次运行/p" > new_gdata.txt
 #使用临时文件中的数据替换原脚本中的内容
-sed -i "/echo 'Please note: GitHub is part of Microsoft';/,/Install #第一次运行/{/echo 'Please note: GitHub is part of Microsoft';/b;/Install #第一次运行/!d;};/Install #第一次运行/r new_data.txt" Gitbackup.sh
+sed -i "/echo 'Please note: GitHub is part of Microsoft';/,/Install #第一次运行/{/echo 'Please note: GitHub is part of Microsoft';/b;/Install #第一次运行/!d;};/Install #第一次运行/r new_gdata.txt" Gitbackup.sh
 if [ $? -eq 0 ]; then
     echo "Gitbackup.sh脚本更新成功!"
 	# 删除多余的行
@@ -10,5 +10,5 @@ if [ $? -eq 0 ]; then
 else
     echo "更新失败!请检查Gitbackup.sh文件是否存在，如果不存在请手动使用cd /脚本所在的目录"
 fi
-rm new_data.txt
+rm new_gdata.txt
 rm GitbakUpdate.sh
